81e6d708fd5e0496afc470e1e14c6b4775000b15
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-11-26 14:06:39 +0800

    11/26/22

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  // content: ["./*.html"],
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
     // Custom Tailwind Breakpoints
     screen: {
